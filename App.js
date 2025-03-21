d960a117b3d7a21e4eb0ad61db7a4af129c7f209
livraria <nicollasfsmoreira@gmail.com>
2025-03-21 15:57:07 -0300

    font adicionada ao projeto

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './src/components/Header';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! teste</Text>
+      <Text style={{ fontFamily: "Gilda Regular", fontWeight: 1000}}>Open up App example text bla bla bla TESTE</Text>
       <StatusBar style="auto" />
     </View>
   );
