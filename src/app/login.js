8d0a4b9f2b1bcd91d97779bb97e1e72b713ec293
sthefany <sthefany.santiago@aluno.ifsp.edu.br>
2025-04-29 17:58:14 -0300

    Primeiro comit.

---
+++ src/app/login.js
@@ -0,0 +1,16 @@
+import { View, StyleSheet, Text } from "react-native";
+import { Image } from "expo-image";
+
+export default function Login() {
+    return (
+        <View style={styles.container}>
+            <Text>oi</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    conatiner: {
+        backgroundColor: "#000002"
+    }
+}) 
