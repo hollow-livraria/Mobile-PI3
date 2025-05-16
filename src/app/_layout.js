e9e91b53abf2d8716bbfe61f711c44231fed21e1
sthefany <sthefany.santiago@aluno.ifsp.edu.br>
2025-05-16 15:12:32 -0300

    tamagui consertado e forget pass estatica finalizada

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -19,6 +19,7 @@ export default function Layout() {
         <Stack.Screen name="index" options={{headerShown: false}} />
         <Stack.Screen name="login" options={{headerShown: false}} />
         <Stack.Screen name="cadastro" options={{headerShown: false}} />
+        <Stack.Screen name="forgetPass" options={{headerShown: false}} />
         </Stack>
     </TamaguiProvider>
   )
