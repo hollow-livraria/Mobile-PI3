28caa9b0d5fd4c88a157d2d8ca0ab780e90f5f2b
livraria <nicollasfsmoreira@gmail.com>
2025-05-16 15:23:38 -0300

    rotas do login implementadas

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,26 +1,28 @@
 // app/_layout.tsx
-import { TamaguiProvider } from 'tamagui'
-import config from '../../tamagui.config'
-import { Stack } from 'expo-router'
+import { TamaguiProvider } from "tamagui";
+import config from "../../tamagui.config";
+import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
     <TamaguiProvider config={config}>
       <Stack
         screenOptions={{
-            headerStyle: {
-            backgroundColor: '#FFF',
-            },
-            headerTintColor: '#000',
-            headerTitleStyle: {
-            fontWeight: 'bold',
-            },
-        }}>
-        <Stack.Screen name="index" options={{headerShown: false}} />
-        <Stack.Screen name="login" options={{headerShown: false}} />
-        <Stack.Screen name="cadastro" options={{headerShown: false}} />
-        <Stack.Screen name="forgetPass" options={{headerShown: false}} />
-        </Stack>
+          headerStyle: {
+            backgroundColor: "#FFF",
+          },
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="cadastro" options={{ headerShown: false }} />
+        <Stack.Screen name="forgetPass" options={{ headerShown: false }} />
+        <Stack.Screen name="vitrine" options={{ headerShown: false }} />
+      </Stack>
     </TamaguiProvider>
-  )
+  );
 }
