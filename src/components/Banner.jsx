8ea02f5f72cba2620111f612591e64b91e5c652a
livraria <nicollasfsmoreira@gmail.com>
2025-06-23 15:26:27 -0300

    banner linkando na vitrine

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,22 +1,38 @@
-
 // ᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼                 ᲼╱|、
 //                           (˚ˎ 。7
 //                            |、˜〵
 //                            じしˍ,)ノ
 
-import { View, StyleSheet, Text, ImageBackground } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  Pressable,
+} from "react-native";
 import { Image } from "expo-image";
+import { useRouter } from "expo-router";
 
 export default function Banner() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
-      <ImageBackground style={styles.bannerImg} source={require("../../assets/imgs/vinho_tinto.png")}>
-      <View style={styles.bannerTexts}>
-        <Text style={styles.conheca}>Conheça Nossos:</Text>
-        <Text style={styles.vinhoTinto}>Vinhos Tintos</Text>
-        <Text style={styles.desc}>Intensos, encorpados e repletos de aromas marcantes, os vinhos tintos brasileiros são perfeitos para quem aprecia sabor e tradição. </Text>
-      </View>
-      </ImageBackground>
+      <Pressable onPress={() => router.push("/vitrine")}>
+        <ImageBackground
+          style={styles.bannerImg}
+          source={require("../../assets/imgs/vinho_tinto.png")}>
+          <View style={styles.bannerTexts}>
+            <Text style={styles.conheca}>Conheça Nossos:</Text>
+            <Text style={styles.vinhoTinto}>Vinhos Tintos</Text>
+            <Text style={styles.desc}>
+              Intensos, encorpados e repletos de aromas marcantes, os vinhos
+              tintos brasileiros são perfeitos para quem aprecia sabor e
+              tradição.{" "}
+            </Text>
+          </View>
+        </ImageBackground>
+      </Pressable>
     </View>
   );
 }
@@ -33,18 +49,18 @@ const styles = StyleSheet.create({
   bannerTexts: {
     width: "50%",
     margin: 20,
-    gap: 10
+    gap: 10,
   },
   conheca: {
     fontSize: 12,
-    color: "white"
+    color: "white",
   },
   vinhoTinto: {
     fontSize: 25,
-    color: "white"
+    color: "white",
   },
   desc: {
     fontSize: 15,
-    color: "white"
-  }
+    color: "white",
+  },
 });
